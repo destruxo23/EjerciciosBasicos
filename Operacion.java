c2c07c7fcc4d18daffa10541a35b919b558ee1f4
destruxo23 <sebastianfigueroa135@gmail.com>
2024-08-05 19:06:49 -0400

    Actualizacion

--- Operacion.java
+++ Operacion.java
@@ -4,7 +4,7 @@ public class Operacion {
         int num_dos = 4;
         int resultado = 0;
 
-        resultado = (num_uno + num_dos) / 2;
+        resultado = num_uno + num_dos / 2;
         System.out.println("El resultado es: " + resultado);
     }
 }
