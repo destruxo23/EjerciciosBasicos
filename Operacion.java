fda157db1a9ad256cdc92cad70b4283855ed9b51
destruxo23 <sebastianfigueroa135@gmail.com>
2024-08-05 18:55:31 -0400

    Se agrega el primer programa

---
+++ Operacion.java
@@ -0,0 +1,10 @@
+public class Operacion {
+    public static void main(String args[]) {
+        int num_uno = 4;
+        int num_dos = 4;
+        int resultado = 0;
+
+        resultado = (num_uno + num_dos) / 2;
+        System.out.println("El resultado es: " + resultado);
+    }
+}
