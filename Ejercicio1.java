5435cf26ffa8c0de1ad968a2f07896f4107c40d4
destruxo23 <sebastianfigueroa135@gmail.com>
2024-08-07 15:52:17 -0400

    Matriz bidimensional

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,12 +1,13 @@
 public class Ejercicio1{
     public static void main(String args[]){
+        //Aqui definimos que serán dos filas y dos columnas
         int numeros[][] = new int [2][2];
-
+        //Definimos los numeros con los que rellenaremos
         numeros[0][0] = 5;
         numeros[0][1] = 2;
         numeros[1][0] = 2;
         numeros[1][1] = 5;
-
+        //Imprimimos
         System.out.print("[" + numeros [0][0] + "]");
         System.out.println("[" + numeros [0][1] + "]");
         System.out.print("[" + numeros [1][0] + "]");
