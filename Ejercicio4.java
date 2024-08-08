685cde09dbbd480ad5f7bca0701003a7a077fc40
destruxo23 <sebastianfigueroa135@gmail.com>
2024-08-08 12:21:19 -0400

    Suma mayor de filas

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -6,18 +6,19 @@ public class Ejercicio4{
         {4,6,8},
         {5,4,7} 
         };
-
+        //declaramos una variable para comparar y guardar la suma mayor
         int sumamayor = 0;
 
         //realizamos un for para pasar por las filas
         for (int i = 0; i < matriz.length; i++){
-            //declaramos variable para guardar el resultado de la suma
+            //declaramos variable para guardar el resultado de la suma de las filas
             int sumafila = 0;
             //realizamos otro for para pasar por las columnas
             for(int j = 0; j < matriz[i].length; j++){
-                //guardamos la suma de la primera fila 
+                //guardamos la suma de la fila 
                 sumafila += matriz[i][j];
             }
+            //condicional para solo guardar el resultado mayor
             if(sumafila > sumamayor){
                 sumamayor = sumafila;
             }
